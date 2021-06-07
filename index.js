bd5870bd39ed982c4f79e7c00c930d926dee5deb
will14lee <will14lee@gmail.com>
2021-06-07 17:53:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser= "William Lee";
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+const excitedCurrentUser= currentUser.toUpperCase();
+const excitedWelcomeMessage= `WELCOME TO FLATBOOK, ${excitedCurrentUser}!`;
+const shortCurrentUser= currentUser.slice(0,2);
+const shortGreeting= `Welcome, ${shortCurrentUser}!`;
